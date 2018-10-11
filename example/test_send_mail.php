71432062d01b0add1668515d8ebe01fac0305184
xingqiba <ixqbar@gmail.com>
2018-10-11 18:30:02 +0800

    update example

--- example/test_send_mail.php
+++ example/test_send_mail.php
@@ -3,7 +3,10 @@
 try {
     $redis_handle = new Redis();
     $redis_handle->connect('127.0.0.1', 8699, 30);
-    $result = $redis_handle->hmset('*', ["To" => "174171262@qq.com,xiaochengxu@caoxianjie.com", 'Subject' => "test", 'Content' => '我知道了在弹出的窗口中输入待加入白名单的邮箱地址，点击“确定”完成添加']);
+    $result = $redis_handle->hmset('*', [
+        "To" => "174171262@qq.com,xiaochengxu@caoxianjie.com",
+        'Subject' => "运行测试通过", 'Content' => '恭喜你完成设置，更多功能请及时关注<a href="https://github.com/jonnywang/goRedisMail">goRedisMail</a>']
+    );
     var_dump($result);
 } catch(Throwable $e) {
    echo $e->getMessage() . PHP_EOL;
